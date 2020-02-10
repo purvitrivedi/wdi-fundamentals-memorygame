f851d8a10fa4181aecd4d2239da1f8fd8bf27193
Purvi Trivedi <purvitrivedi@purvis-mbp.home>
2020-02-10 18:49:44 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardTwo);
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped "+ cardOne);
+console.log("User flipped "+ cardTwo);
+
+if(cardsInPlay.length===2) {
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry! Try again!");
+	}
+}
+
