868691d812385bf4f223d27522d7b72c8751be94
Purvi Trivedi <purvitrivedi@purvis-mbp.home>
2020-02-11 16:33:07 +0000

    created game logic with objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,50 @@
 
-const cards = ["queen","queen","king","king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
 
 function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!");
 	}
 	else {
-		console.log("Sorry! Try again!");
+		alert("Sorry! Try again!");
 	}
 }
 
 function flipCard(cardId) {
-	console.log("User flipped "+ cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped "+ cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	if(cardsInPlay.length === 2){
 	checkForMatch();
 }
 }
 
 flipCard(0);
-flipCard(2);
+flipCard(3);
 
 
 
