229c2ddb80b84bd1430bf5410a226d2aaacfe30e
Purvi Trivedi <purvitrivedi@purvis-mbp.home>
2020-02-11 10:50:14 +0000

    created game logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,21 +2,25 @@
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped "+ cardOne);
-console.log("User flipped "+ cardTwo);
-
-if(cardsInPlay.length===2) {
-	if(cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
+function checkForMatch() {
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
 	}
 	else {
-		alert("Sorry! Try again!");
+		console.log("Sorry! Try again!");
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped "+ cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2){
+	checkForMatch();
+}
+}
+
+flipCard(0);
+flipCard(2);
+
+
 
